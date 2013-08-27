2f6f5e3aea538ec63622d25b8542083e4137aa32
Anthony Almarza <anthony.almarza@gmail.com>
2013-08-27 11:06:08 -0400

    something is wrong with using autoreload after pip installing the repo

--- hendrix/hendrix-devserver.py
+++ hendrix/hendrix-devserver.py
@@ -1,6 +1,7 @@
 import os, sys
 import imp
-import autoreload
+# import autoreload
+
 DEPLOYMENT_TYPE = "local"
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.local' #If the try block above did not cause exit, we know that this module exists.
 
@@ -22,7 +23,8 @@ resource, server = get_hendrix_resource(wsgi, 'settings.'+DEPLOYMENT_TYPE, port=
 try:
     server.startService()
     print ("Listening on port %s" % PORT)
-    autoreload.main(reactor.run())
+    reactor.run()
+    # autoreload.main(reactor.run())
 except CannotListenError, e:
     thread_pool = server.services[0].pool
     thread_pool.stop()
