44194c2c3e10b469c1dd59a81f80dca9cbd56c02
Anthony Almarza <anthony.almarza@gmail.com>
2013-08-27 07:08:38 -0400

    because life shouldn't be difficult

--- hendrix/hendrix-devserver.py
+++ hendrix/hendrix-devserver.py
@@ -1,5 +1,6 @@
 import os, sys
 import imp
+import autoreload
 DEPLOYMENT_TYPE = "local"
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.local' #If the try block above did not cause exit, we know that this module exists.
 
@@ -21,7 +22,7 @@ resource, server = get_hendrix_resource(wsgi, 'settings.'+DEPLOYMENT_TYPE, port=
 try:
     server.startService()
     print ("Listening on port %s" % PORT)
-    reactor.run()
+    autoreload.main(reactor.run())
 except CannotListenError, e:
     thread_pool = server.services[0].pool
     thread_pool.stop()
