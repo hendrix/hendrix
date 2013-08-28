b86d405e57f1be1c7f3d6b2a95064c669502ebd6
Anthony Almarza <anthony.almarza@gmail.com>
2013-08-28 17:16:15 -0400

    virtualenv path needs to be set to get autoreload to work

--- hendrix/hendrix-devserver.py
+++ hendrix/hendrix-devserver.py
@@ -1,13 +1,14 @@
+from hendrix.path_settings import * #Just to set the appropriate sys.path
+
 import os, sys
 import imp
-# import autoreload
+import autoreload
 
 DEPLOYMENT_TYPE = "local"
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.local' #If the try block above did not cause exit, we know that this module exists.
 
 from twisted.internet import reactor
 from hendrix.deploy_functions import get_hendrix_resource
-from hendrix.path_settings import * #Just to set the appropriate sys.path
 from twisted.internet.error import CannotListenError
 
 try:
@@ -23,8 +24,7 @@ resource, server = get_hendrix_resource(wsgi, 'settings.'+DEPLOYMENT_TYPE, port=
 try:
     server.startService()
     print ("Listening on port %s" % PORT)
-    reactor.run()
-    # autoreload.main(reactor.run())
+    autoreload.main(reactor.run())
 except CannotListenError, e:
     thread_pool = server.services[0].pool
     thread_pool.stop()
