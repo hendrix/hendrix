0cb875c7c45bfdccb5866667a87650b5312db058
Anthony Almarza <anthony.almarza@gmail.com>
2014-04-19 15:54:06 -0400

    fixes caching bugs

--- hendrix/contrib/cache/resource.py
+++ hendrix/contrib/cache/resource.py
@@ -3,15 +3,27 @@ from datetime import datetime
 import gzip
 import urlparse
 
-from hendrix.defaults import MAX_AGE
+from hendrix.defaults import DEFAULT_MAX_AGE
 
 from urllib import quote as urlquote
+from time import strptime
 from twisted.internet import reactor, defer, threads
-from twisted.web import proxy, resource
+from twisted.web import proxy, resource, client
 from twisted.web.server import NOT_DONE_YET
 
 PREFIX = "/CACHE"
 
+
+def processURI(uri, prefix=''):
+    """
+    helper function to return just the path (uri) and whether or not it's busted
+    """
+    components = urlparse.urlparse(uri)
+    query = dict(urlparse.parse_qsl(components.query))
+    bust = True
+    bust &= bool(query)  # bust the cache if the query has stuff in it
+    bust &= query.get('cache') != 'true'  # bust the cache if the query key 'cache' isn't true
+    return prefix + components.path, bust
 
 
 class CacheClient(proxy.ProxyClient):
@@ -25,7 +37,7 @@ class CacheClient(proxy.ProxyClient):
         )
         self.resource = resource
         self.buffer = cStringIO.StringIO()
-
+        self._response = None
 
     def closeBuffer(self):
         self.buffer.close()
@@ -39,40 +51,71 @@ class CacheClient(proxy.ProxyClient):
     def dataReceived(self, transport):
         proxy.ProxyClient.dataReceived(self, transport)
 
+    def handleHeader(self, key, value):
+        "extends handleHeader to save headers to a local response object"
+        if key.lower() == 'location':
+            value = self.modLocationPort(value, 8000)
+        proxy.ProxyClient.handleHeader(self, key, value)
+        self._response.headers[key.lower()] = value
+
+    def handleStatus(self, version, code, message):
+        "extends handleStatus to instantiate a local response object"
+        # if int(code) != 301:
+        proxy.ProxyClient.handleStatus(self, version, code, message)
+        # client.Response is currently just a container for needed data
+        self._response = client.Response(version, code, message, {}, None)
+
+    def modLocationPort(self, location, port):
+        "ensures that the location port is a the given port value"
+        components = urlparse.urlparse(location)
+        _value = getattr(components, 'port')
+        if _value != port:
+            _components = components._asdict()  # returns an ordered dict of urlparse.ParseResult components
+            _components['netloc'] = '%s:%d' % (components.hostname, port)
+        return urlparse.urlunparse(_components.values())
+
     def handleResponseEnd(self):
+        """
+        Extends handleResponseEnd to not care about the user closing/refreshing
+        their browser before the response is finished. Also calls cacheContent
+        in a thread that we don't care when it finishes.
+        """
         try:
             if not self._finished:
                 content = self.buffer.getvalue()
-                reactor.callInThread(self.cache, content, self.father)
+                reactor.callInThread(self.cacheContent, content, self.father)
             proxy.ProxyClient.handleResponseEnd(self)
         except RuntimeError:
             # because we don't care if the user hits
             # refresh before the request is done
             pass
 
-    def cache(self, content, request):
-
-        cache_control = self.headers.get('cache-control')
-        if cache_control:
-            parsed = urlparse.parse_qsl(cache_control)
-            if parsed:
-                max_age_name, max_age = parsed[0]
-                max_age = int(max_age)
-            else:
-                max_age = MAX_AGE
-        else:
-            max_age = MAX_AGE
-        if max_age and request.method == "GET" and request.code/100 == 2:
-            content = self.compressBuffer(content)
-            self.resource.putCache(content, PREFIX + request.uri, max_age)
+    def cacheContent(self, content, request):
+        """
+        Caches the content in a gzipped format given that a `cache_it` flag is
+        True
+        """
+        code = int(self._response.code)
+        cache_it = False
+        # only cache the content if it was successful and requested using GET
+        uri, bust = processURI(request.uri, PREFIX)
+        if request.method == "GET" and code/100 == 2 and not bust:
+            cache_control = self._response.headers.get('cache-control')
+            if cache_control:
+                params = dict(urlparse.parse_qsl(cache_control))
+                if int(params.get('max-age', '0')) > 0:
+                    cache_it = True
+            if cache_it:
+                content = self.compressBuffer(content)
+                self.resource.putCache(content, uri, self._response.headers)
         self.closeBuffer()
 
 
     def handleResponsePart(self, buffer):
         """
+        Sends the content to the browser and keeps a local copy of it.
         buffer is just a str of the content to be shown, father is the intial
         request.
-        Compresses and stores web responses associated to uri's
         """
         self.father.write(buffer)
         self.buffer.write(buffer)
@@ -115,10 +158,10 @@ class CachedResource(resource.Resource):
 
     isLeaf = True
 
-    def __init__(self, content=None, max_age=None):
+    def __init__(self, content=None, headers=None):
         resource.Resource.__init__(self)
         self.content = content
-        self.max_age = max_age
+        self.headers = headers
         self.created = datetime.now()
 
     def decompressBuffer(self, buffer):
@@ -135,7 +178,46 @@ class CachedResource(resource.Resource):
     def render(self, request):
         return self.content
 
+    def getMaxAge(self):
+        "get the max-age in seconds from the saved headers data"
+        max_age = 0
+        cache_control = self.headers.get('cache_control')
+        if cache_control:
+            params = dict(urlparse.parse_qsl(cache_control))
+            max_age = int(params.get('max-age', '0'))
+        return max_age
 
+    def convertTimeString(self, timestr):
+        """
+        Returns a datetime instance from a str formatted as follows
+            e.g. 'Mon, 03 Mar 2014 12:12:12 GMT'
+        """
+        time_struc = strptime(timestr, '%a, %d %b %Y %H:%M:%S GMT')
+        return datetime.datetime(*time_struc[:6])
+
+    def getLastModified(self):
+        "returns the GMT last-modified datetime or None"
+        last_modified = self.headers.get('last-modified')
+        if last_modified:
+            last_modified = self.convertTimeString(last_modified)
+        return last_modified
+
+    def getDate(self):
+        "returns the GMT response datetime or None"
+        date = self.headers.get('date')
+        if date:
+            date = self.convertTimeString(date)
+        return date
+
+    def isFresh(self):
+        "returns True if cached object is still fresh"
+        max_age = self.getMaxAge()
+        date = self.getDate()
+        is_fresh = False
+        if max_age and date:
+            delta_time = datetime.now() - date
+            is_fresh = delta_time.total_seconds() < max_age
+        return is_fresh
 
 class CacheProxyResource(proxy.ReverseProxyResource):
     """
@@ -168,13 +250,15 @@ class CacheProxyResource(proxy.ReverseProxyResource):
         """
         # start caching logic
         is_secure = request.isSecure()
-        uri = PREFIX + request.uri
-        if uri in self.children and not is_secure and request.method == "GET":
+        uri, bust = processURI(request.uri, PREFIX)  # the prefix namespaces these resources
+        # Reasons not to bother looking in the Cache
+        #     * it's been busted
+        #     * it's a secure request
+        #     * it's not a GET request
+        #     * it's not actually in the cache
+        if uri in self.children and not is_secure and request.method == "GET" and not bust:
             child = self.children[uri]
-            created = child.created
-            max_age = child.max_age
-            delta_time = datetime.now() - created
-            is_fresh = delta_time.total_seconds() < max_age
+            is_fresh = child.isFresh()
             if is_fresh:
                 encodings = request.getHeader('accept-encoding')
                 if encodings and 'gzip' in encodings:
@@ -243,6 +327,10 @@ class CacheProxyResource(proxy.ReverseProxyResource):
                 pass
         return
 
-    def putCache(self, content, url, max_age):
-        resource = CachedResource(content, max_age)
+    def putCache(self, content, url, headers):
+        """
+        instantiates the CachedResource and adds to the global
+        ReverseProxyResource children
+        """
+        resource = CachedResource(content, headers)
         self.putChild(url, resource)
