f0cc8f20cca02350c29c1d623289537f1baadd5a
Anthony Almarza <anthony.almarza@gmail.com>
2014-04-29 12:09:47 -0400

    gets pip install register and sdist to work as expected

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from path import path
 from setuptools import setup, find_packages
 
 
-def long_desc():
-    with open(os.path.join(path(__file__).parent, 'docs/long_desc.rst')) as f:
+def read(rel_path):
+    with open(os.path.join(path(__file__).parent, rel_path)) as f:
         ret = f.read()
+    return ret
+
+def readlines(rel_path):
+    with open(os.path.join(path(__file__).parent, rel_path)) as f:
+        ret = f.readlines()
     return ret
 
 
@@ -27,17 +32,17 @@ setup(
     author_email = "hendrix@reelio.com",
     name = "hendrix",
     packages = find_packages(),
-    version = "v0.1.1-beta",
+    version = "0.1.1",
     url = "https://github.com/hangarunderground/hendrix",
     download_url = "https://github.com/hangarunderground/hendrix/tarball/v0.1.1-beta",
     description = "A deployment module for Django that uses Twisted.",
-    long_description = long_desc(),
+    long_description = read('docs/long_desc.html'),
     classifiers = [
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: Linux Unix',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
     ],
@@ -47,5 +52,5 @@ setup(
         ('/usr/local/bin', ['hendrix/utils/scripts/install-hendrix-service',]),
         ('/usr/local/share/hendrix', ['hendrix/utils/templates/init.d.j2',]),
     ],
-    install_requires = open('requirements').readlines(),
+    install_requires = readlines('requirements')
 )
