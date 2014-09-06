d1ebe8af288ed9c356267f278f62a8177c1653aa
Damon C <gdamon@gmail.com>
2014-09-05 17:11:20 -0700

    added example django chatserver app

---
+++ examples/django_hx_chatserver/example_app/example_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def home(request, chat_channel_name=None):
+
+    """
+        if we have a chat_channel_name kwarg,
+        have the response include that channel name
+        so the javascript knows to subscribe to that
+        channel...
+    """
+
+    if not chat_channel_name:
+        chat_channel_name = 'homepage'
+
+    ctx = {'address': chat_channel_name}
+
+    return render(request, 'base.html', ctx)
