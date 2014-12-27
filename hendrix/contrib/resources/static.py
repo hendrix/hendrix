4131a026135b6051c3a017507cada9496a35dbec
Damon C <gdamon@gmail.com>
2014-12-27 13:12:40 -0500

    adds DjangoStaticsFinder which creates a DjangoStaticResource for each statics directory found by django's get_finders method

--- hendrix/contrib/resources/static.py
+++ hendrix/contrib/resources/static.py
@@ -1,8 +1,41 @@
 import os
 from hendrix.resources import DjangoStaticResource
 from django.conf import settings
+from django.contrib.staticfiles import finders
 
 
+
+class DjangoStaticsFinder:
+    """ 
+        finds all static resources for this django installation
+        and creates a static resource for each's base directory 
+    """
+
+    namespace = settings.STATIC_URL
+
+    @staticmethod
+    def get_resources():
+
+        ignore_patterns = [
+            '*.less',
+            '*.scss',
+            '*.styl',
+        ]
+
+        existing = []
+        for finder in finders.get_finders():
+            for _, storage in finder.list([]):
+                if not storage.base_location in existing:
+                    yield DjangoStaticResource(
+                        storage.location,
+                        settings.STATIC_URL + '%s/' % storage.base_location
+                    )
+
+"""
+The rest is for compatibility with existing code based on the deprecated
+classes below.
+
+"""
 try:
     DefaultDjangoStaticResource = DjangoStaticResource(
         settings.STATIC_ROOT, settings.STATIC_URL
@@ -21,3 +54,4 @@ try:
     )
 except:
     raise
+
