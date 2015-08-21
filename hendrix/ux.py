919f0f77b4fe91a96f96a185c68958d0e4f2bb59
jMyles <justin@justinholmes.com>
2015-08-21 17:16:14 -0400

    Moving ssl module to late import.  Otherwise this module can't be imported on pypy25, which breaks the travis tests.

--- hendrix/ux.py
+++ hendrix/ux.py
@@ -13,7 +13,7 @@ import time
 import traceback
 from .options import HendrixOptionParser, cleanOptions
 from hendrix.contrib import SettingsError
-from hendrix.deploy import base, ssl, cache, hybrid
+from hendrix.deploy import base, cache, hybrid
 from hendrix.logger import hendrixObserver
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -79,6 +79,7 @@ def launch(*args, **options):
             if options['key'] and options['cert'] and options['cache']:
                 HendrixDeploy = hybrid.HendrixDeployHybrid
             elif options['key'] and options['cert']:
+                from hendrix.deploy import ssl
                 HendrixDeploy = ssl.HendrixDeploySSL
             elif options['cache']:
                 HendrixDeploy = cache.HendrixDeployCache
